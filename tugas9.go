b39dad0fe0856b3befb84abb372b4fb4865086c9
Ditaanggraeni <72622857+Ditaanggraeni@users.noreply.github.com>
2021-09-15 20:51:23 +0700

    tugas9

---
+++ tugas9.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+func validasi(input string) (bool, error) {
+	if strings.TrimSpace(input) == "" {
+		return false, errors.New("Menu tidak boleh kosong")
+	}
+	return true, nil
+}
+
+var menu, next string
+
+func main() {
+	menus()
+	input()
+	pesan()
+	fmt.Println("Terimakasih atas pesanannya")
+
+}
+
+func menus() {
+	fmt.Println("Toko Makanan Indonesia")
+	fmt.Println("==========================")
+	fmt.Println("Tahu")
+	fmt.Println("Tempe")
+	fmt.Println("Sambal")
+	fmt.Println("Nasi")
+	fmt.Println("Lele")
+	fmt.Println("Ayam")
+	fmt.Println("==========================")
+}
+
+func input() {
+	fmt.Print("Masukkan menu pesanan anda dalam huruf ( eg: Tahu ) : ")
+	fmt.Scanln(&menu)
+
+	if valid, err := validasi(menu); valid {
+		defer fmt.Println("Pesanan anda : ", menu)
+	} else {
+		fmt.Println(err.Error())
+	}
+	fmt.Print("Lanjutkan Memesan ? (Y/T) ")
+	fmt.Scanln(&next)
+
+	pesan()
+}
+
+func pesan() {
+	if next == "y" {
+		input()
+
+	} else {
+		fmt.Print()
+	}
+}
